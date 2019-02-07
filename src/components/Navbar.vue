a8b1531dc2bff0a3d4f397ba4ed478a58ae71438
William Kilburg <william.kilburg@gmail.com>
2019-02-07 22:02:09 +0100

    vue-gallery added

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,7 +4,7 @@
       <v-toolbar-title class="text-uppercase">
         <span class="white--text">S</span>
         <span class="hidden-xs-only font-weight-light white--text">tuttgart</span>
-        <span class="white--text text--darken-2">21 </span>
+        <span class="white--text text--darken-2">21</span>
         <span class="grey--text text--darken-2">Webcams</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
@@ -14,27 +14,26 @@
           <v-icon left>expand_more</v-icon>
           <span class="white--text">Menü</span>
         </v-btn>
-        <v-list class="red white--text"  >
+        <v-list class="red white--text">
           <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
-            <v-list-tile-title >{{ link.text }}</v-list-tile-title>
+            <v-list-tile-title>{{ link.text }}</v-list-tile-title>
           </v-list-tile>
         </v-list>
       </v-menu>
-
     </v-toolbar>
   </nav>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        links: [
-          {icon: 'linked_camera', text: 'Webcams', route: '/'},
-          {icon: 'not_listed_location', text: 'Über mich', route: '/about'}
-        ]
-      }
-    }
+export default {
+  data() {
+    return {
+      links: [
+        { icon: "linked_camera", text: "Webcams", route: "/" },
+        { icon: "not_listed_location", text: "Über mich", route: "/about" }
+      ]
+    };
   }
+};
 </script>
 
--- src/views/Webcam.vue
+++ src/views/Webcam.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="Webcam">
+    <gallery :images="getImage(webcams)" :index="index" @close="index = null"></gallery>
     <v-layout justify-space-around row fill-height wrap>
-      <v-flex xs12 sm6 md4 v-for="cam in webcams" :key="cam.url">
+      <v-flex xs12 sm6 md4 v-for="cam in webcams" :key="cam.id">
         <v-hover>
           <v-card slot-scope="{ hover }" class="red ma-3" hover>
             <v-img :src="cam.url" aspect-ratio="1.5">
               <v-expand-transition>
-                <div
-                  v-if="hover"
-                  class="d-flex transition-ease-in-out red v-card--reveal display-4"
-                  style="height: 15%;"
-                >
-                  <v-icon class="white--text" big>zoom_in</v-icon>
+                <div v-if="hover" class="d-flex transition-ease-in-out red v-card--reveal display-4" style="height: 15%;">
+                  <v-icon class="white--text" @click="index = cam.id" big>zoom_in</v-icon>
                 </div>
               </v-expand-transition>
             </v-img>
@@ -19,7 +16,7 @@
             <v-card-title>
               <v-layout align-center justify-space-around row fill-height>
                 <v-flex shrink>
-                  <h1 class="title white--text">{{ cam.name }}</h1>
+                  <h1 class="title white--text">{{ cam.name }}</h1>                  
                 </v-flex>
 
                 <v-flex grow>
@@ -43,13 +40,26 @@
 
 <script>
 import json from "@/data/webcams.json";
+import VueGallery from "vue-gallery";
 
 export default {
+  components: {
+    // eslint-disable-next-line
+    gallery: VueGallery
+  },
   data() {
     return {
-      webcams: json.webcams
+      webcams: json.webcams,
+      images: [],
+      index: null
     };
+  },
+  methods: {
+    getImage(webcams){
+      return this.webcams.map(cam => cam.url);
+    }
   }
+
 };
 </script>
 
