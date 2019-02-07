2a6b87c21e7147cf2af0364600bee42d4b3eb234
William Kilburg <william.kilburg@sidion.de>
2019-02-07 13:09:17 +0100

    hover effect on card added

--- src/views/Webcam.vue
+++ src/views/Webcam.vue
@@ -2,30 +2,40 @@
   <div class="Webcam">
     <v-layout justify-space-around row fill-height wrap>
       <v-flex xs12 sm6 md4 v-for="cam in webcams" :key="cam.url">
-        
-        <v-card class="red ma-3" hover>
-          <v-img :src="cam.url" aspect-ratio="1.5"></v-img>
+        <v-hover>
+          <v-card slot-scope="{ hover }" class="red ma-3" hover>
+            <v-img :src="cam.url" aspect-ratio="1.5">
+              <v-expand-transition>
+                <div
+                  v-if="hover"
+                  class="d-flex transition-ease-in-out red v-card--reveal display-4"
+                  style="height: 15%;"
+                >
+                  <v-icon class="white--text" big>zoom_in</v-icon>
+                </div>
+              </v-expand-transition>
+            </v-img>
 
-          <v-card-title>
-            <v-layout align-center justify-space-around row fill-height>
-              <v-flex shrink>
-                <h1 class="title white--text">{{ cam.name }}</h1>
-              </v-flex>
+            <v-card-title>
+              <v-layout align-center justify-space-around row fill-height>
+                <v-flex shrink>
+                  <h1 class="title white--text">{{ cam.name }}</h1>
+                </v-flex>
 
-              <v-flex grow>
-                <v-spacer></v-spacer>
-              </v-flex>
+                <v-flex grow>
+                  <v-spacer></v-spacer>
+                </v-flex>
 
-              <v-flex shrink>
-                <v-chip small right class="caption red--text white">
-                  <v-icon left>label</v-icon>
-                  {{cam.category}}
-                </v-chip>
-              </v-flex>
-            </v-layout>
-          </v-card-title>
-        </v-card>
-                
+                <v-flex shrink>
+                  <v-chip small right class="caption red--text white">
+                    <v-icon left>label</v-icon>
+                    {{cam.category}}
+                  </v-chip>
+                </v-flex>
+              </v-layout>
+            </v-card-title>
+          </v-card>
+        </v-hover>
       </v-flex>
     </v-layout>
   </div>
@@ -42,3 +52,14 @@ export default {
   }
 };
 </script>
+
+<style>
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: 0.7;
+  position: absolute;
+  width: 100%;
+}
+</style>
