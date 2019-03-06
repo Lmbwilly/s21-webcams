66943a664a3a4e5d5839e975e4db605f7580b724
William Kilburg <william.kilburg@gmail.com>
2019-03-06 18:16:25 +0100

    footer added with infos to the webcams

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,16 @@
     <v-content class="ma-4">
       <router-view></router-view>
     </v-content>
+    <v-footer color="red" height="auto">
+      <v-flex lighten-2 text-xs-center white--text xs12>
+        <strong>
+          Die Webcams werden vom Bahnprojekt Stuttgart–Ulm bereit gestellt, die Webcam-Übersicht finden Sie unter
+          <a
+            href="http://www.bahnprojekt-stuttgart-ulm.de/baustelle/webcams/"
+          >http://www.bahnprojekt-stuttgart-ulm.de/baustelle/webcams/</a>
+        </strong>
+      </v-flex>
+    </v-footer>
   </v-app>
 </template>
 
