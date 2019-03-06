3e151cc616342e576894c0e77e9fed9a52bc3776
William Kilburg <william.kilburg@gmail.com>
2019-03-06 17:59:13 +0100

    Navbar icons added

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,7 +16,10 @@
         </v-btn>
         <v-list class="red white--text">
           <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
-            <v-list-tile-title>{{ link.text }}</v-list-tile-title>
+            <v-list-tile-title>
+              <v-icon color="white">{{link.icon}}</v-icon>
+              {{ link.text }}
+            </v-list-tile-title>
           </v-list-tile>
         </v-list>
       </v-menu>
